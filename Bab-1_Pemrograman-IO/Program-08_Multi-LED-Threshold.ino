7ca3cd6e7d937578aba1de8ffa39fed49a575516
ImNotDanish05 <danishmahdi2005@gmail.com>
2026-09-11 15:08:11 +0700

    feat: Update program 08

--- Bab-1_Pemrograman-IO/Program-08_Multi-LED-Threshold.ino
+++ Bab-1_Pemrograman-IO/Program-08_Multi-LED-Threshold.ino
@@ -26,19 +26,26 @@ void setup()
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
   int nilaiPot = analogRead(potPin);
 
-  if (nilaiPot > threshold)
+  Serial.print("Nilai Potensiometer: ");
+  Serial.print(nilaiPot);
+
+  if (nilaiPot < threshold)
   {
     // Potensiometer di atas threshold → LED menyala
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
     digitalWrite(led3, LOW);
     digitalWrite(led4, LOW);
+
+    Serial.println(" | LED: NYALA");
   }
   else
   {
@@ -47,5 +54,9 @@ void loop()
     digitalWrite(led2, HIGH);
     digitalWrite(led3, HIGH);
     digitalWrite(led4, HIGH);
+
+    Serial.println(" | LED: MATI");
   }
+
+  delay(100);
 }
